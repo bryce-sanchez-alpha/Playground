f5d8b7f91a6072bb9ade788b1bad9d2eb0e935c1
Bryce Sanchez <bryce.sanchez@alphalab.capital>
2025-07-22 15:54:29 +0800

    Verb

--- Python_2025-07-22/main.py
+++ Python_2025-07-22/main.py
@@ -1,8 +1,12 @@
-def gcd(a:int, b:int) -> int:
-    a,b = abs(a),abs(b)
-    a,b = max(a,b),min(a,b)
-    while b != 0:
-        a,b = b,a%b
-    return a
+import sys
 
-print(gcd(12,10))
+print(sys.version)
+
+# def gcd(a:int, b:int) -> int:
+#     a,b = abs(a),abs(b)git@github.com:bryce-sanchez-alpha/Playground.git
+#     a,b = max(a,b),min(a,b)
+#     while b != 0:
+#         a,b = b,a%b
+#     return a
+#
+# print(gcd(12,10))
